c397106bd6a9dd2fa75f146688edf782fe832651
MagnusLaid <laid.magnus@gmail.com>
2023-06-05 16:50:45 +0300

    Change port to 10000

--- index.js
+++ index.js
@@ -5,7 +5,15 @@
         res.send('Simple API homepage');
     });
 
-    app.listen(5070, () => {
-        console.log("Server running on port 5070");
+        app.get('/api/treks', (req, res) => {
+        res.send('Sending a list of items from the DB ...')
+    });
+
+    app.post('/api/treks', (req, res) => {
+        res.status(201).send('Sent the new data to the DB ...')
+    });
+
+    app.listen(10000, () => {
+        console.log("Server running on port 10000");
     });
 
